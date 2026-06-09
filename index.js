7b0d41d57ef52e5edd1d8a0b16366bd5e0892273
bill <zhangbiao-zb@outlook.com>
2026-06-09 19:34:21 +0800

    Set HidenCloud fantasy pet upstream default

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 import { startCommunityApiServer } from "./services/community-api/src/server.js";
 
-startCommunityApiServer();
+startCommunityApiServer({
+  env: {
+    ...process.env,
+    PORT: process.env.PORT ?? "24674",
+    FANTASY_PET_API_BASE_URL:
+      process.env.FANTASY_PET_API_BASE_URL ?? "http://120.48.67.110:8765"
+  }
+});
